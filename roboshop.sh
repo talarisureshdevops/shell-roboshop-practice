e4507adc3f738f986f43e6c2f9ed982a1dc55a01
Talari <suresh.talari-ext@hpe.com>
2026-05-26 11:54:45 +0530

    added new one

---
+++ roboshop.sh
@@ -0,0 +1,55 @@
+SG_ID="sg-07718e2f0115aadf5"
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z0692071PCP0NH1SHCGI"
+DOMAIN_NAME="laddudevops86.fun"
+
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+    fi
+
+    echo "IP Address: $IP"
+
+ aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+{
+  "Comment": "Creating an A record for my website",
+  "Changes": [
+    {
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "'$RECORD_NAME'",
+        "Type": "A",
+        "TTL": 300,
+        "ResourceRecords": [
+          { "Value": "'$IP'" }
+        ]
+      }
+    }
+  ]
+}
